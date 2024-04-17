7ab8f3b620074f63bf1f5e2967de01c4821f6ba1
Miteos1986 <mdmitriy1986@gmail.com>
2024-04-18 00:41:05 +0200

    load

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ let page = 1;
 let per_page = 15;
 
 searchForm.addEventListener("submit", handleSubmit);
-// buttonLoad.addEventListener("click", nextPage);
+buttonLoad.addEventListener("click", nextPage);
 
 
 async function handleSubmit (event){
@@ -30,13 +30,13 @@ event.preventDefault();
 loader.classList.remove("hide");
 list.innerHTML="";
 const input = searchForm.elements.picture.value;
+
 page = 1;
 searchForm.reset();
 
 try {
   
  const data = await searchPhoto (input, page);
- 
      if (data.hits.length === 0) {
       buttonLoad.style.display = "none";
       loaderOff();
@@ -45,10 +45,17 @@ try {
         message: 'Enter text'
         });
     } else { 
-    
-  list.insertAdjacentHTML("beforeend",createMarkup(data.hits));
+      
+      list.insertAdjacentHTML("beforeend",createMarkup(data.hits));
   lightbox.refresh();
   page += 1;
+  // const totalPages = Math.ceil(data.totalHits / per_page);
+// console.log(totalPages);
+//   if (page < totalPages) {
+    
+      buttonLoad.hidden = false;
+    // }
+   
 }
 
 }
@@ -67,6 +74,33 @@ try {
 
 async  function nextPage (event) {
 
+  try {
+    const input = searchForm.elements.picture.value;
+    const data = await searchPhoto (input, page);
+
+    const totalPages = Math.ceil(data.totalHits / per_page);
+
+  list.insertAdjacentHTML("beforeend",createMarkup(data.hits));
+  lightbox.refresh();
+  page += 1;
+  if (page >= totalPages) { 
+    buttonLoad.style.display = "none";
+    iziToast.show({
+    position: "topRight",
+    message: "We're sorry, but you've reached the end of search results."
+  })
+}}
+    catch(error)  {
+  
+      loaderOff();
+        iziToast.error({
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please try again!'
+        });
+      }
+      finally {
+        loaderOff();
+      }
 }
 
 function loaderOff (){
